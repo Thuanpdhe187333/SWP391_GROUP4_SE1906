76e83d34d9d6b99391658a62413050a8d4078f9e
Duongnd07 <duongndhe187234@fpt.edu.vn>
2025-05-24 21:45:57 +0700

    Update Product.java

--- Product.java
+++ Product.java
@@ -15,22 +15,25 @@ public class Product {
     private String description;
     private double price;
     private int quantity;
-    private int categoryId;
-    private Timestamp createAt;
+    private String image;
+    private int categoryID;
     private Integer labelID;
+    private Timestamp createAt;
+    
 
     public Product() {
     }
 
-    public Product(int productID, String productName, String description, double price, int quantity, int categoryId, Timestamp createAt, Integer labelID) {
+    public Product(int productID, String productName, String description, double price, int quantity, String image, int categoryID, Integer labelID, Timestamp createAt) {
         this.productID = productID;
         this.productName = productName;
         this.description = description;
         this.price = price;
         this.quantity = quantity;
-        this.categoryId = categoryId;
-        this.createAt = createAt;
+        this.image = image;
+        this.categoryID = categoryID;
         this.labelID = labelID;
+        this.createAt = createAt;
     }
 
     public int getProductID() {
@@ -73,12 +76,28 @@ public class Product {
         this.quantity = quantity;
     }
 
-    public int getCategoryId() {
-        return categoryId;
+    public String getImage() {
+        return image;
     }
 
-    public void setCategoryId(int categoryId) {
-        this.categoryId = categoryId;
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(int categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public Integer getLabelID() {
+        return labelID;
+    }
+
+    public void setLabelID(Integer labelID) {
+        this.labelID = labelID;
     }
 
     public Timestamp getCreateAt() {
@@ -89,15 +108,11 @@ public class Product {
         this.createAt = createAt;
     }
 
-    public Integer getLabelID() {
-        return labelID;
+    @Override
+    public String toString() {
+        return "Product{" + "productID=" + productID + ", productName=" + productName + ", description=" + description + ", price=" + price + ", quantity=" + quantity + ", image=" + image + ", categoryID=" + categoryID + ", labelID=" + labelID + ", createAt=" + createAt + '}';
     }
 
-    public void setLabelID(Integer labelID) {
-        this.labelID = labelID;
-    }
-    
-    
 }
 
 
