d782d31fa77cd16e92277e5b4c880e4cd4d07c77
Thuanpdhe187333 <thuanpdhe187333@fpt.edu.vn>
2025-06-26 22:05:05 +0700

    Add files via upload

---
+++ src/java/model/Rating.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+public class Rating {
+    private int id;
+    private int userId;
+    private int productId;
+    private int rating;
+
+    public Rating() {
+    }
+
+    public Rating(int id, int userId, int productId, int rating) {
+        this.id = id;
+        this.userId = userId;
+        this.productId = productId;
+        this.rating = rating;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    @Override
+    public String toString() {
+        return "Rating{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", productId=" + productId +
+                ", rating=" + rating +
+                '}';
+    }
+}
+
